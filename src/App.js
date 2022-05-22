3fdbaab931c08a7f1101a09a5c4a49e2589fe42c
GlebSergey1985 <gleb_55rus@mail.ru>
2022-05-22 22:21:55 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import{button} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <header>
-            <div class="header">
-                <div class="city">
-                     <a href="#">City</a>
-                </div>
-                <div class="empty">
-
-                </div>
-                  <div clss="document">
-                    <a href='#'>Document</a>
-                 </div>
-                 <div class="contakt">
-                    <a href="#">Contakt</a>
-                 </div>
-                  <div class="download">
-                    <a href="#">Download</a>
-                  </div>
-                
-            </div>    
-        </header>   
+      <div class="left">
+        <a href="#">City</a>
+      </div>
+      <div class="right">
+        <a href='#'>Document</a>
+        <a href="#">Contact</a>
+        <a href="#">Download</a>
+      </div>
+    </header>   
   );
 }
 
